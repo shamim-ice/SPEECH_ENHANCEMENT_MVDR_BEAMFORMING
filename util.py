bcd607f6d568fd96624c239d4316603dcdecf1cd
shamim <shamim.ice.iu@gmai.com>
2022-09-19 15:48:43 +0600

    Showed spectrogram of noisy signal

--- util.py
+++ util.py
@@ -1,1 +1,128 @@
-print("SPEECH ENHANCEMENT WITH MVDR BEAMFORMING")
+import torch
+import torchaudio
+import torchaudio.functional as F
+import torchaudio.transforms as T
+
+
+from pesq import pesq
+from pystoi import stoi
+import mir_eval
+import numpy
+
+import matplotlib.pyplot as plt
+from IPython.display import Audio, display
+from torchaudio.utils import download_asset
+
+SAMPLE_RATE = 16000
+SAMPLE_CLEAN = download_asset("tutorial-assets/mvdr/clean_speech.wav")
+SAMPLE_NOISE = download_asset("tutorial-assets/mvdr/noise.wav")
+
+
+def plot_spectrogram(stft, title="Spectrogram", xlim=None):
+    magnitude = stft.abs()
+    spectrogram = 20 * torch.log10(magnitude + 1e-8).numpy()
+    figure, axis = plt.subplots(1, 1)
+    img = axis.imshow(spectrogram, cmap="viridis", vmin=-100, vmax=0, origin="lower", aspect="auto")
+    figure.suptitle(title)
+    plt.colorbar(img, ax=axis)
+    plt.show()
+
+
+def plot_mask(mask, title="Mask", xlim=None):
+    mask = mask.numpy()
+    figure, axis = plt.subplots(1, 1)
+    img = axis.imshow(mask, cmap="viridis", origin="lower", aspect="auto")
+    figure.suptitle(title)
+    plt.colorbar(img, ax=axis)
+    plt.show()
+
+
+def si_snr(estimate, reference, epsilon=1e-8):
+    estimate = estimate - estimate.mean()
+    reference = reference - reference.mean()
+    reference_pow = reference.pow(2).mean(axis=1, keepdim=True)
+    mix_pow = (estimate * reference).mean(axis=1, keepdim=True)
+    scale = mix_pow / (reference_pow + epsilon)
+
+    reference = scale * reference
+    error = estimate - reference
+
+    reference_pow = reference.pow(2)
+    error_pow = error.pow(2)
+
+    reference_pow = reference_pow.mean(axis=1)
+    error_pow = error_pow.mean(axis=1)
+
+    si_snr = 10 * torch.log10(reference_pow) - 10 * torch.log10(error_pow)
+    return si_snr.item()
+
+
+def generate_mixture(waveform_clean, waveform_noise, target_snr):
+    power_clean_signal = waveform_clean.pow(2).mean()
+    power_noise_signal = waveform_noise.pow(2).mean()
+    current_snr = 10 * torch.log10(power_clean_signal / power_noise_signal)
+    waveform_noise *= 10 ** (-(target_snr - current_snr) / 20)
+    return waveform_clean + waveform_noise
+
+
+def evaluate(estimate, reference):
+    si_snr_score = si_snr(estimate, reference)
+    (
+        sdr,
+        _,
+        _,
+        _,
+    ) = mir_eval.separation.bss_eval_sources(reference.numpy(), estimate.numpy(), False)
+    pesq_mix = pesq(SAMPLE_RATE, estimate[0].numpy(), reference[0].numpy(), "wb")
+    stoi_mix = stoi(reference[0].numpy(), estimate[0].numpy(), SAMPLE_RATE, extended=False)
+    print(f"SDR score: {sdr[0]}")
+    print(f"Si-SNR score: {si_snr_score}")
+    print(f"PESQ score: {pesq_mix}")
+    print(f"STOI score: {stoi_mix}")
+
+
+def play_audio(waveform, sample_rate):
+    waveform = waveform.numpy()
+
+    num_channels, num_frames = waveform.shape
+    print(num_channels)
+    if num_channels == 1:
+        display(Audio(waveform[0], rate=sample_rate))
+    elif num_channels == 8:
+        display(Audio((waveform[0], waveform[1],waveform[2], waveform[3],waveform[4], waveform[5],waveform[6], waveform[7]), rate=sample_rate))
+    else:
+        raise ValueError("Waveform with more than 2 channels are not supported")
+
+
+
+
+waveform_clean, sr = torchaudio.load(SAMPLE_CLEAN)
+#metadata = torchaudio.info(waveform_clean)
+#print(waveform_clean)
+waveform_noise, sr2 = torchaudio.load(SAMPLE_NOISE)
+assert sr == sr2 == SAMPLE_RATE
+# The mixture waveform is a combination of clean and noise waveforms with a desired SNR.
+target_snr = 3
+waveform_mix = generate_mixture(waveform_clean, waveform_noise, target_snr)
+
+waveform_mix = waveform_mix.to(torch.double)
+waveform_clean = waveform_clean.to(torch.double)
+waveform_noise = waveform_noise.to(torch.double)
+
+N_FFT = 1024
+N_HOP = 256
+stft = torchaudio.transforms.Spectrogram(
+    n_fft=N_FFT,
+    hop_length=N_HOP,
+    power=None,
+)
+istft = torchaudio.transforms.InverseSpectrogram(n_fft=N_FFT, hop_length=N_HOP)
+
+stft_mix = stft(waveform_mix)
+stft_clean = stft(waveform_clean)
+stft_noise = stft(waveform_noise)
+
+plot_spectrogram(stft_mix[0], "Spectrogram of Mixture Speech (dB)")
+#evaluate(waveform_mix[0:1], waveform_clean[0:1])
+
+Audio(waveform_mix[0], rate=SAMPLE_RATE)
